3a850435e261efccb3482da41a98b8dc7f98fa04
NObodyGX <nobodygx@163.com>
2024-05-10 16:01:07 +0800

    demo 1

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
-mod application;
-mod gui;
+mod task_object;
+mod task_row;
+mod window;
 
-use application::AsciiboxApplication;
-use gtk::{gio, glib, prelude::*};
+use gtk::prelude::*;
+use gtk::{gio, glib, Application};
+use window::Window;
 
-// temp test
 pub static PKGDATADIR: &str = "../data";
 
+// ANCHOR: main
 fn main() -> glib::ExitCode {
-    // Load resources
+    // Register and include resources
     let resources = gio::Resource::load(PKGDATADIR.to_owned() + "/asciibox.gresource")
         .expect("Could not load resources");
     gio::resources_register(&resources);
 
-    let app = AsciiboxApplication::default();
+    // Create a new application
+    let app = Application::builder()
+        .application_id("com.github.nobodygx.asciibox")
+        .build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
     app.run()
 }
+
+fn build_ui(app: &Application) {
+    // Create a new custom window and present it
+    let window = Window::new(app);
+    window.present();
+}
+// ANCHOR_END: main
