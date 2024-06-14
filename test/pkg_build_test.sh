06a18071aae692d86e0abd8d66000123c0b183cd
NObodyGX <nobodygx@163.com>
2024-06-14 17:39:59 +0800

    add pkg_test script

---
+++ test/pkg_build_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pwd=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+tdir="${pwd}/pkg_build"
+
+if [ ! -d ${tdir} ];then
+  mkdir -p ${tdir}
+fi
+
+cp ${pwd}/../PKGBUILD ${tdir}/PKGBUILD
+
+cd ${tdir}
+
+updpkgsums
+
+makepkg --printsrcinfo > .SRCINFO
+
+makepkg
+
+cd -
+
+
+
