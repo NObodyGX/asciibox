2b2128f0cbf03e6cdca253f99b79f0d83cebbb61
NObodyGX <nobodygx@163.com>
2024-08-07 10:39:55 +0800

    up ignore

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
-pub static VERSION: &str = "0.1.2";
-pub static GETTEXT_PACKAGE: &str = "asciibox";
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
-pub static PKGDATADIR: &str = "/usr/local/share/asciibox";
+pub const VERSION: &str = "0.8.4";
+pub const PKGDATA_DIR: &str = "/usr/local/share/asciibox";
+pub const APP_ID: &str = "com.github.nobodygx.asciibox";
+pub const APP_NAME: &str = "asciibox";
+pub const APP_URL: &str = "https://github.com/nobodygx/asciibox";
